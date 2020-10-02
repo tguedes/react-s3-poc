3134133025b8af30b3f6e03169ff2f165ea8ad69
Thiago Guedes <thiagoguedes@Thiagos-MBP.localdomain>
2020-10-01 20:32:31 -0400

    fixing sync

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aws s3 sync ./build s3://react-s3-poc-bravado-health
+aws s3 sync ./build s3://react-s3-poc-bravado-health --acl public-read
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changing Website
+          New Test
         </a>
       </header>
     </div>
